035ab0414a564c3d11f40eca2407a426205f85c8
wkosla <w.kosla@gmail.com>
2019-03-10 12:55:15 +0100

    Add to top button

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,203 +1,202 @@
 const svg = d3.select('.skills__svg'),
   data = [],
   forces = [],
-  orbs = [
-    {
-      label: 'Sass', // SASS
-      background: 'grad-purple',
-      radius: {
-        xs: 132,
-        sm: 132,
-        md: 176
-      },
-      position: {
-        xs: [500, 150],
-        sm: [1000, 150],
-        md: [1000, 80]
-      }
+  orbs = [{
+    label: 'Sass', // SASS
+    background: 'grad-purple',
+    radius: {
+      xs: 132,
+      sm: 132,
+      md: 176
     },
-    {
-      label: '',
-      background: 'grad-light',
-      radius: {
-        xs: 108,
-        sm: 108,
-        md: 144
-      },
-      position: {
-        xs: [300, 220],
-        sm: [800, 220],
-        md: [750, 200]
-      }
-    },
-    {
-      label: 'jQuery', //JQUERY
-      background: 'grad-darkpurple',
-      radius: {
-        xs: 132,
-        sm: 132,
-        md: 176
-      },
-      position: {
-        xs: [80, 560],
-        sm: [550, 550],
-        md: [420, 650]
-      }
-    },
-    {
-      label: '',
-      background: 'grad-light',
-      radius: {
-        xs: 192,
-        sm: 192,
-        md: 256
-      },
-      position: {
-        xs: [580, 670],
-        sm: [1060, 670],
-        md: [1100, 800]
-      }
-    },
-    {
-      label: 'GIT', // GIT
-      background: 'grad-red',
-      radius: {
-        xs: 132,
-        sm: 132,
-        md: 176
-      },
-      position: {
-        xs: [350, 1640],
-        sm: [1350, 900],
-        md: [1500, 1100]
-      }
-    },
-    {
-      label: 'HTML5', // HTML5
-      background: 'grad-orange',
-      radius: {
-        xs: 108,
-        sm: 108,
-        md: 144
-      },
-      position: {
-        xs: [510, 950],
-        sm: [1220, 750],
-        md: [1300, 900]
-      }
-    },
-    {
-      label: '',
-      background: 'grad-light',
-      radius: {
-        xs: 120,
-        sm: 120,
-        md: 160
-      },
-      position: {
-        xs: [430, 790],
-        sm: [920, 800],
-        md: [900, 950]
-      }
-    },
-    {
-      label: 'JS', // JAVASCRIPT
-      background: 'grad-yellow',
-      radius: {
-        xs: 216,
-        sm: 216,
-        md: 288
-      },
-      position: {
-        xs: [370, 500],
-        sm: [850, 500],
-        md: [830, 560]
-      }
-    },
-    {
-      label: '',
-      background: 'grad-light',
-      radius: {
-        xs: 144,
-        sm: 144,
-        md: 192
-      },
-      position: {
-        xs: [-20, 920],
-        sm: [450, 900],
-        md: [250, 1100]
-      }
-    },
-    {
-      label: 'Vue', // VUE
-      background: 'grad-green',
-      radius: {
-        xs: 192,
-        sm: 192,
-        md: 256
-      },
-      position: {
-        xs: [170, 1150],
-        sm: [650, 1120],
-        md: [500, 1400]
-      }
-    },
-    {
-      label: 'PS', // PHOTOSHOP
-      background: 'grad-darkblue',
-      radius: {
-        xs: 108,
-        sm: 108,
-        md: 144
-      },
-      position: {
-        xs: [570, 1250],
-        sm: [150, 1000],
-        md: [-200, 1300]
-      }
-    },
-    {
-      label: '',
-      background: 'grad-light',
-      radius: {
-        xs: 132,
-        sm: 132,
-        md: 176
-      },
-      position: {
-        xs: [450, 1420],
-        sm: [1450, 400],
-        md: [1600, 400]
-      }
-    },
-    {
-      label: '',
-      background: 'grad-light',
-      radius: {
-        xs: 0,
-        sm: 0,
-        md: 224
-      },
-      position: {
-        xs: [0, 0],
-        sm: [0, 0],
-        md: [1550, -30]
-      }
-    },
-    {
-      label: 'CSS3', // CSS3
-      background: 'grad-lightblue',
-      radius: {
-        xs: 108,
-        sm: 108,
-        md: 144
-      },
-      position: {
-        xs: [320, 1300],
-        sm: [1300, 250],
-        md: [1400, 200]
-      }
+    position: {
+      xs: [500, 150],
+      sm: [1000, 150],
+      md: [1000, 80]
     }
+  },
+  {
+    label: '',
+    background: 'grad-light',
+    radius: {
+      xs: 108,
+      sm: 108,
+      md: 144
+    },
+    position: {
+      xs: [300, 220],
+      sm: [800, 220],
+      md: [750, 200]
+    }
+  },
+  {
+    label: 'jQuery', //JQUERY
+    background: 'grad-darkpurple',
+    radius: {
+      xs: 132,
+      sm: 132,
+      md: 176
+    },
+    position: {
+      xs: [80, 560],
+      sm: [550, 550],
+      md: [420, 650]
+    }
+  },
+  {
+    label: '',
+    background: 'grad-light',
+    radius: {
+      xs: 192,
+      sm: 192,
+      md: 256
+    },
+    position: {
+      xs: [580, 670],
+      sm: [1060, 670],
+      md: [1100, 800]
+    }
+  },
+  {
+    label: 'GIT', // GIT
+    background: 'grad-red',
+    radius: {
+      xs: 132,
+      sm: 132,
+      md: 176
+    },
+    position: {
+      xs: [350, 1640],
+      sm: [1350, 900],
+      md: [1500, 1100]
+    }
+  },
+  {
+    label: 'HTML5', // HTML5
+    background: 'grad-orange',
+    radius: {
+      xs: 108,
+      sm: 108,
+      md: 144
+    },
+    position: {
+      xs: [510, 950],
+      sm: [1220, 750],
+      md: [1300, 900]
+    }
+  },
+  {
+    label: '',
+    background: 'grad-light',
+    radius: {
+      xs: 120,
+      sm: 120,
+      md: 160
+    },
+    position: {
+      xs: [430, 790],
+      sm: [920, 800],
+      md: [900, 950]
+    }
+  },
+  {
+    label: 'JS', // JAVASCRIPT
+    background: 'grad-yellow',
+    radius: {
+      xs: 216,
+      sm: 216,
+      md: 288
+    },
+    position: {
+      xs: [370, 500],
+      sm: [850, 500],
+      md: [830, 560]
+    }
+  },
+  {
+    label: '',
+    background: 'grad-light',
+    radius: {
+      xs: 144,
+      sm: 144,
+      md: 192
+    },
+    position: {
+      xs: [-20, 920],
+      sm: [450, 900],
+      md: [250, 1100]
+    }
+  },
+  {
+    label: 'Vue', // VUE
+    background: 'grad-green',
+    radius: {
+      xs: 192,
+      sm: 192,
+      md: 256
+    },
+    position: {
+      xs: [170, 1150],
+      sm: [650, 1120],
+      md: [500, 1400]
+    }
+  },
+  {
+    label: 'PS', // PHOTOSHOP
+    background: 'grad-darkblue',
+    radius: {
+      xs: 108,
+      sm: 108,
+      md: 144
+    },
+    position: {
+      xs: [570, 1250],
+      sm: [150, 1000],
+      md: [-200, 1300]
+    }
+  },
+  {
+    label: '',
+    background: 'grad-light',
+    radius: {
+      xs: 132,
+      sm: 132,
+      md: 176
+    },
+    position: {
+      xs: [450, 1420],
+      sm: [1450, 400],
+      md: [1600, 400]
+    }
+  },
+  {
+    label: '',
+    background: 'grad-light',
+    radius: {
+      xs: 0,
+      sm: 0,
+      md: 224
+    },
+    position: {
+      xs: [0, 0],
+      sm: [0, 0],
+      md: [1550, -30]
+    }
+  },
+  {
+    label: 'CSS3', // CSS3
+    background: 'grad-lightblue',
+    radius: {
+      xs: 108,
+      sm: 108,
+      md: 144
+    },
+    position: {
+      xs: [320, 1300],
+      sm: [1300, 250],
+      md: [1400, 200]
+    }
+  }
   ];
 
 let size;
@@ -252,7 +251,7 @@ data.forEach((data, i) => {
   generateOrbs(data, forces[i], i, orbs);
 });
 
-svg.on('mousemove', function() {
+svg.on('mousemove', function () {
   let p1 = d3.mouse(this);
 
   data.forEach((data, i) => {
