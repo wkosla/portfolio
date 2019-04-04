9fbc7fe8acb40029558c07f85c989b49a2ef828f
Wojciech Kosla <wkosla@antmicro.com>
2019-04-04 17:39:29 +0200

    Moving orbs back to svg but using images

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,18 +1,11 @@
-const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 500,
-    speedAsDuration: true
-  }),
-  width = 2000,
-  height = 1500,
-  canvas = d3.select('.skills__canvas').attr('width', width).attr('height', height),
-  context = canvas.node().getContext('2d'),
+const svg = d3.select('.skills__svg'),
   data = [],
   forces = [],
   orbs = [{
     label: 'Sass', // SASS
     colors: ['#cb00cb', '#6711a5'],
     file: '../assets/img/balls/sass.svg',
-    width: 353 / 2,
+    width: 353,
     radius: 176,
     position: [1200 + 1000, 80 + 620]
   },
@@ -20,7 +13,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: '',
     colors: ['#fff', '#e6f0fa'],
     file: '../assets/img/balls/ball-5.svg',
-    width: 321 / 2,
+    width: 321,
     radius: 144,
     position: [1200 + 750, 200 + 620]
   },
@@ -28,7 +21,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: 'Gulp', //Gulp
     colors: ['#ff0051', '#940000'],
     file: '../assets/img/balls/gulp.svg',
-    width: 353 / 2,
+    width: 353,
     radius: 176,
     position: [1200 + 420, 650 + 620]
   },
@@ -36,7 +29,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: '',
     colors: ['#fff', '#e6f0fa'],
     file: '../assets/img/balls/ball-4.svg',
-    width: 433 / 2,
+    width: 433,
     radius: 256,
     position: [1200 + 1100, 800 + 620]
   },
@@ -44,7 +37,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: 'GIT', // GIT
     colors: ['#f3008c', '#8a003e'],
     file: '../assets/img/balls/git.svg',
-    width: 353 / 2,
+    width: 353,
     radius: 176,
     position: [1200 + 1500, 1100 + 620]
   },
@@ -52,7 +45,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: 'HTML5', // HTML5
     colors: ['#ffa700', '#ff4200'],
     file: '../assets/img/balls/html.svg',
-    width: 322 / 2,
+    width: 322,
     radius: 144,
     position: [1200 + 1300, 900 + 620]
   },
@@ -60,7 +53,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: '',
     colors: ['#fff', '#e6f0fa'],
     file: '../assets/img/balls/ball-1.svg',
-    width: 337 / 2,
+    width: 337,
     radius: 160,
     position: [1200 + 900, 950 + 620]
   },
@@ -68,7 +61,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: 'JS', // JAVASCRIPT
     colors: ['#ffdf00', '#ff9100'],
     file: '../assets/img/balls/javascript.svg',
-    width: 466 / 2,
+    width: 466,
     radius: 288,
     position: [1200 + 830, 560 + 620]
   },
@@ -76,7 +69,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: '',
     colors: ['#fff', '#e6f0fa'],
     file: '../assets/img/balls/ball-3.svg',
-    width: 370 / 2,
+    width: 370,
     radius: 192,
     position: [1200 + 250, 1100 + 620]
   },
@@ -84,7 +77,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: 'Vue', // VUE
     colors: ['#bdff16', '#59a52b'],
     file: '../assets/img/balls/vue.svg',
-    width: 433 / 2,
+    width: 433,
     radius: 256,
     position: [1200 + 500, 1400 + 620]
   },
@@ -92,7 +85,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: 'Webpack', // WEBPACK
     colors: ['#3174fa', '#1246c9'],
     file: '../assets/img/balls/webpack.svg',
-    width: 321 / 2,
+    width: 321,
     radius: 144,
     position: [1200 + -200, 1300 + 620]
   },
@@ -100,7 +93,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: '',
     colors: ['#fff', '#e6f0fa'],
     file: '../assets/img/balls/ball-2.svg',
-    width: 354 / 2,
+    width: 354,
     radius: 176,
     position: [1200 + 1600, 400 + 620]
   },
@@ -108,7 +101,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: '',
     colors: ['#fff', '#e6f0fa'],
     file: '../assets/img/balls/ball-6.svg',
-    width: 401 / 2,
+    width: 401,
     radius: 224,
     position: [1200 + 1550, -30 + 620]
   },
@@ -116,7 +109,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     label: 'CSS3', // CSS3
     colors: ['#00e1ff', '#008cff'],
     file: '../assets/img/balls/css.svg',
-    width: 321 / 2,
+    width: 321,
     radius: 144,
     position: [1200 + 1400, 200 + 620]
   }
@@ -134,75 +127,46 @@ function generateOrbs(data, force, index, orbs) {
   force.size(orbs[index].position);
   force.start();
 
+  const circle = svg.selectAll('#circle-' + index);
+
+  circle
+    .data(data.nodes.slice(1))
+    .enter()
+    .append('image')
+    .attr('xlink:href', orbs[index].file)
+    .attr('width', orbs[index].width)
+    .attr('height', orbs[index].width);
+
   force.on('tick', () => {
-    const d = data.nodes[1],
-      grad = context.createRadialGradient(
-        d.x + (d.radius * .30),
-        d.y - (d.radius * .30),
-        0,
-        d.x + (d.radius * .30),
-        d.y - (d.radius * .30),
-        d.radius * 1.35);
-  
-    grad.addColorStop(0, orbs[index].colors[0]);
-    grad.addColorStop(1, orbs[index].colors[1]);
-
-    if (index === 0) {
-      context.clearRect(0, 0, width, height);
-    }
-    let img = new Image();
-    img.src = orbs[index].file;
-    context.drawImage(img, d.x - orbs[index].width, d.y - orbs[index].width);
-    context.fill();
+    svg.selectAll('image')
+      .attr('x', (d) => {
+        return d.x - orbs[index].width;
+      })
+      .attr('y', (d) => d.y - orbs[index].width);
   });
 }
 
-function skillOrbsInit() {
-  if (this.innerWidth < 1024) return false;
+for (let i = 0; i < orbs.length; i++) {
+  data[i] = {
+    nodes: generateNodes(orbs[i].radius / 2)
+  };
+  data[i].root = data[i].nodes[0];
+  data[i].root.radius = 0;
+  data[i].root.fixed = true;
 
-  for (let i = 0; i < orbs.length; i++) {
-    data[i] = {
-      nodes: generateNodes(orbs[i].radius / 2)
-    };
-    data[i].root = data[i].nodes[0];
-    data[i].root.radius = 0;
-    data[i].root.fixed = true;
-  
-    forces[i] = d3.layout.force().gravity(0.5).charge((d, i) => (i ? 0 : (-1 * orbs[i].radius * 20))).nodes(data[i].nodes);
-  }
-  
+  forces[i] = d3.layout.force().gravity(0.7).charge((d, i) => (i ? 0 : (-1 * orbs[i].radius * 20))).nodes(data[i].nodes);
+}
+
+data.forEach((data, i) => {
+  generateOrbs(data, forces[i], i, orbs);
+});
+
+svg.on('mousemove', function () {
+  let p1 = d3.mouse(this);
+
   data.forEach((data, i) => {
-    generateOrbs(data, forces[i], i, orbs);
+    data.root.px = p1[0];
+    data.root.py = p1[1];
+    forces[i].resume();
   });
-  
-  canvas.on('mousemove', function () {
-    let p1 = d3.mouse(this);
-  
-    data.forEach((data, i) => {
-      data.root.px = p1[0];
-      data.root.py = p1[1];
-      forces[i].resume();
-    });
-  });
-}
-
-skillOrbsInit();
-
-const main = document.getElementById('page'),
-  nav = document.querySelector('.nav'),
-  navBtn = document.querySelector('.nav__btn'),
-  navLinks = document.querySelectorAll('.nav a');
-
-function navToggle() {
-  if (window.innerWidth >= 768) return false;
-  main.classList.toggle('page--hidden');
-  nav.classList.toggle('nav--open');
-}
-
-navBtn.addEventListener('click', navToggle);
-navLinks.forEach(link => link.addEventListener('click', () => {
-  if (nav.classList.contains('nav--open')) {
-    navToggle();
-  }
-}));
-
+});
